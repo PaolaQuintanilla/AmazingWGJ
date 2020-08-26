8e9a461d67113e2424b04cf2ed1bb68e8f89fc7e
Daniela Emilse Cordova Villca <decvdaniemi@gmail.com>
2020-08-26 09:32:51 -0400

    script de movimiento del personaje y seguidor

--- Amazing/Assets/Script/MovimientoPersonaje.cs
+++ Amazing/Assets/Script/MovimientoPersonaje.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
+    
+    //velocidad: es con la velociadad que se va a mover el personaje 
+    public float velocidad=5f;
+    
+    public float contadorTiempo=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,38 @@ public class MovimientoPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //para rotar al personaje 
+        #region Rotacion
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.rotation= Quaternion.Euler(0,0,0);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            transform.rotation= Quaternion.Euler(180,0,0);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation= Quaternion.Euler(0,0,90);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation= Quaternion.Euler(0,0,-90);
+        }
+        #endregion
         
+        //para que se pueda mover al personaje 
+        #region caminar
+        if(
+            Input.GetKey(KeyCode.W) || 
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D)
+        )
+        {
+            transform.Translate(0,velocidad*Time.deltaTime,0);
+        }
+        contadorTiempo=contadorTiempo+Time.deltaTime;
+        #endregion
     }
 }
---
+++ Amazing/Assets/Script/Seguidor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguidor : MonoBehaviour
+{
+    
+    //objetivo: es el personaje o objeto que va a seguir
+    public Transform objetivo;
+    //suabidad: con que suabidad se a mover la seguidor 
+    public float suabidad=5f;
+
+    void Update()
+    {
+        //para que la seguidor se mueva donde esta el personaje 
+        transform.position=Vector3.Lerp(transform.position,objetivo.position,suabidad*Time.deltaTime);
+    }
+}
